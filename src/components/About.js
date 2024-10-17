a14870c4a2e385529279328b3cd546c98c1d0808
Prakhar5355 <workwithprakhar27@gmail.com>
2024-10-17 22:16:41 +0530

    useNavigate hook

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,12 @@ import React, { useContext } from 'react';
 import { UserContext } from '../utils/UserContext'; // Adjust the import path as necessary
 import { addUser } from '../utils/userSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router';
 
 const About = () => {
   const { user, changeEmail, changeName } = useContext(UserContext);
   const users = useSelector((state) => state.user.users);
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleEmailChange = () => {
     const newEmail = prompt("Enter new email:", user.email);
@@ -34,6 +36,7 @@ const About = () => {
       <button onClick={handleEmailChange}>Change Email</button>
       <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleAddUser}>Add</button>
+      <button onClick={()=>navigate('/contact')}>Contact</button>
     </>
   );
 };
